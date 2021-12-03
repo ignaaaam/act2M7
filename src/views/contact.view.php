3832f2d07f6f394174b47ba1558f82e5eb99b162
iamatnuria <ignasiamat10@gmail.com>
2021-12-03 16:08:09 +0100

    First commit

--- src/views/contact.view.php
+++ src/views/contact.view.php
@@ -12,6 +12,7 @@
         <br>
         <label class="sr-only"  for="message">Message:</label>    
         <textarea class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mb-8" rows="6" name="message" id="contact-message" placeholder="Enter your message..."></textarea>
+        <button type="button" class="flex mx-auto mt-2 mb-4 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg cursor-pointer border border-white-100 border-opacity-50">Enviar</button>
         
     </form>
 </div>
